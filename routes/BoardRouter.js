5f016dc9da91a1af20f9cb7e81b0c57f9a682eb0
Cesar <cesar.decasasgarcia@gmail.com>
2020-12-06 15:37:36 -0800

    board router finished

--- routes/BoardRouter.js
+++ routes/BoardRouter.js
@@ -5,6 +5,7 @@ Router.post('/:user_id', controller.createBoard)
 Router.delete('/:board_id', controller.deleteBoard)
 Router.put('/:board_id', controller.updateBoard)
 Router.get('/:board_id', controller.getBoard)
+Router.get('/user/:user_id', controller.getUserBoards)
 
 
 module.exports = Router
