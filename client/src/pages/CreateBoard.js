b45ed5e107afb0d3351c2dc9870d1b8e084bb463
raquelgothardt <71895033+raqgoth@users.noreply.github.com>
2020-12-07 13:47:51 -0500

    createBoard.js page

--- client/src/pages/CreateBoard.js
+++ client/src/pages/CreateBoard.js
@@ -12,5 +12,49 @@ const validators = (value)=> {
     return errors
 }
 
+const NewBoard = () => {
+    const [loading,setLoading] = useState(false);
+    const [message,setMessage] = useState('');
+    const history = useHistory();
 
-      
+    const onSubmit = values=>{
+        const {title} = values;
+        setLoading(true);
+        BoardService.create({title})
+        .then(res=>{
+            setLoading(false);
+            setMessage(res.data.message);
+            setInterval(function(){ 
+                history.push('/');
+            },2000);
+        });
+    }
+    return (
+        <div className="Board">
+            <Nav title="Boards" />
+            <div className="container mt-5">
+                <div className="row">
+                    <div className="col-md-6 offset-md-3 col-sm-12">
+                        { message!==''? (<div className="alert alert-success" >{message}</div>) :''}
+                        <h3>New Board</h3>
+                        <Form
+                        validate={validators}
+                        onSubmit={onSubmit}
+                        render={(formProps) =>(
+                            <div>
+                                <CustomField name="name" inputType="text" label="name"  placeholder="name..." />
+                                <button type="submit" className="btn btn-success" onClick={formProps.handleSubmit} disabled={loading}>
+                                    {loading ? ('Creating post...'): ('Create')}
+                                </button>
+                            </div>
+                            
+                        )}
+                        />  
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default NewBoard
