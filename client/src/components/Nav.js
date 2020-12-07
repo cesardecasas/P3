34f028f08dce2231d369eddc3ba60a6de16b94ff
benjmain roth <benjaminsavanne@gmail.com>
2020-12-07 11:43:17 -0600

    update nav

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
-import '../styles/Nav.css'
+// import '../styles/Nav.css'
 
 
 const Nav = (authenticated, currentUser)=>{
@@ -9,12 +9,13 @@ const Nav = (authenticated, currentUser)=>{
     return !authenticated && !currentUser ? (
         <header >
             <nav className='navbar  '>
+            <NavLink className="btn btn-outline-success" to='/home' >
+                    Home
+                </NavLink>
                 <NavLink className="btn btn-outline-success" to='/' >
                     About Us
                 </NavLink>
-                <NavLink className="btn btn-outline-success" to='/signup'>
-                    Sign Up
-                </NavLink>
+                
                 <NavLink className="btn btn-outline-success" to='/login'>
                     Log In
                 </NavLink>
@@ -23,7 +24,13 @@ const Nav = (authenticated, currentUser)=>{
     ) : (
         <header>
             <nav>
-                <NavLink className="btn btn-outline-success" to='/home' >
+                <NavLink className="btn btn-outline-success" to='/signup'>
+                    Sign Up
+                </NavLink>
+                <NavLink className="btn btn-outline-success" to='/login'>
+                    Log In
+                </NavLink>
+                <NavLink className="btn btn-outline-success" to='/' >
                     Home
                 </NavLink>
                 <NavLink className="btn btn-outline-success" onClick={() => localStorage.clear()} 
