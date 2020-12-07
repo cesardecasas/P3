5700dcbc25690970bf652bb985bdb755debc0e34
Cesar <cesar.decasasgarcia@gmail.com>
2020-12-07 08:27:47 -0800

    step routes

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -2,10 +2,12 @@ const Router = require('express').Router()
 const UserRouter = require('./UserRouter')
 const BoardRouter= require('./BoardRouter')
 const TaskRouter = require('./TasksRouter')
+const StepRouter = require('./StepRouter')
 
 Router.use('/user', UserRouter)
 Router.use('/board', BoardRouter)
 Router.use('/task', TaskRouter)
+Router.use('/step', StepRouter)
 
 
 module.exports = Router
