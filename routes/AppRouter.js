a919e34131b6d9a64cf08a04e6ce809896f6454e
Cesar <cesar.decasasgarcia@gmail.com>
2020-12-07 07:52:46 -0800

    task routes ready

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -1,7 +1,11 @@
 const Router = require('express').Router()
 const UserRouter = require('./UserRouter')
+const BoardRouter= require('./BoardRouter')
+const TaskRouter = require('./TasksRouter')
 
 Router.use('/user', UserRouter)
+Router.use('/board', BoardRouter)
+Router.use('/task', TaskRouter)
 
 
 module.exports = Router
