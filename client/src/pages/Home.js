65c9e4332e2c2b4af656a9580071a05b9f7a65e3
benjmain roth <benjaminsavanne@gmail.com>
2020-12-08 14:32:28 -0600

    add css and logo to home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React from "react"
+import logo from '../helpers/PThreeLogo.png'
+import '../styles/Home.css'
 
-const Home = () => {
+const Home = (props) => {
     try{
-
+        console.log(props)
     } catch(error){
         console.log(error)
     }
     return(
-        <h3>Hello World</h3>
+        <div className='entire-container'>
+            <div className='intro-container'>
+                <div className='intro'>
+                    <h2>
+                        Let's Get It Done!
+                    </h2>
+                    <p>
+                        Sticky Board!
+                    </p>
+                </div>
+                <div className='intro-footer'>
+                    <img className='image'  src={logo} />
+                    <p><span>Welcome Back, {props.currentUser.name}</span></p>
+                </div>
+                
+            </div>
+        </div>
     )
 }
 
 export default Home
+
+
+
