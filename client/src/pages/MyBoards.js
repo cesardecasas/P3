1618f03979128674a79304527ddec820e21fd9b5
benjmain roth <benjaminsavanne@gmail.com>
2020-12-11 00:51:03 -0600

    adjust css on myboards

--- client/src/pages/MyBoards.js
+++ client/src/pages/MyBoards.js
@@ -41,7 +41,7 @@ const MyBoards = (props)=>{
 
     return(
         <div className= 'title'>
-            {boards ? <h1>My Boards</h1> : <h1>Create Your First Board!</h1>}
+            {boards ? <div className='board-header'><h1>My Boards</h1></div> : <div className='board-header'><h1>Create Your First Board!</h1></div>}
             <div className='crd-container'>
                     {boards.map((board,index)=>{
                         
@@ -61,8 +61,8 @@ const MyBoards = (props)=>{
                                     <h3 class="card-header">{board.name}</h3>
                                 </Link>
                                 
-                                <button className='delete' onClick = {()=> DeleteBoard(board.id)}>delete</button>
-                                <button className='delete' onClick = {()=> UpdateBoard()} > update</button>
+                                <img href='https://images.costco-static.com/ImageDelivery/imageService?profileId=12026540&itemId=719046-847&recipeName=680' className='delete' onClick = {()=> DeleteBoard(board.id)} />
+                                <img  src='https://image.flaticon.com/icons/png/512/124/124123.png' className='update' onClick = {()=> UpdateBoard()} />
                                 <div class="card-body">
                                     <h5 class="card-title">Columns Name</h5>
                                     {board.Tasks.map(task=><p className='card-text'>{task.name}</p>)}
@@ -75,12 +75,13 @@ const MyBoards = (props)=>{
                                     <Link to={location} key={board.name}>
                                         <h3 className="card-header">{board.name}</h3>
                                     </Link>
-                                    
-                                    <button className='delete' onClick = {()=> DeleteBoard(board.id)}>delete</button>
+                                    <div className='board-crud-image-container'>
+                                         <img src='https://www.netclipart.com/pp/m/75-756943_eraser-tool-pixelated-basketball.png' className='eraser' onClick = {()=> DeleteBoard(board.id)} />
+                                         <img src='https://banner2.cleanpng.com/20180331/rve/kisspng-apple-pencil-pixel-art-pixel-art-5ac02c99308f97.1806420915225437691989.jpg' className='pencil' onClick = {()=> UpdateBoard(board.id)} />
+                                    </div>     
                                     <Link to={location2} key={board.name}>
                                         
                         
-                                    <button className='delete' onClick = {()=> UpdateBoard(board.id)} > update</button>
                                     </Link>
                                     <div className="card-body">
                                         <h5 className="card-title">Columns Name</h5>
